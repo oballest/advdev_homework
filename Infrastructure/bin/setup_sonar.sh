d117c071e2cf84b69713b9ad3491c74642c1216d
Oscar Ballesteros <oballest@oballest.remote.fedora>
2019-04-08 08:24:18 -0500

    infraestructure componets resources limitation due deployments failures

--- Infrastructure/bin/setup_sonar.sh
+++ Infrastructure/bin/setup_sonar.sh
@@ -15,3 +15,11 @@ echo "Setting up Sonarqube in project $GUID-sonarqube"
 
 # To be Implemented by Student
 oc new-app --file=Infrastructure/templates/sonarqube-template.yml --param SONAR_DB_USER=sonar --param SONAR_DB_PASSWORD=sonar --param SONAR_DB_NAME=sonar -n ${GUID}-sonarqube
+
+while : ; do
+   echo "Checking if Sonarqube is Ready..."
+   oc get pod -n ${GUID}-sonarqube|grep '\-1\-'|grep -v postgres|grep -v deploy|grep "1/1"
+   [[ "$?" == "1" ]] || break
+   echo "...no. Sleeping 10 seconds."
+   sleep 10
+done
